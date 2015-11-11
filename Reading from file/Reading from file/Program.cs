2a7168c8daef1276798a6b109050341ac1777261
Shawon Ashraf <shawon13@live.com>
2015-11-11 19:45:49 +0600

    File IO in C#

---
+++ Reading from file/Reading from file/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Reading_from_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file;
+            StreamReader reader;
+            string data = ""; // an empty string
+
+            try
+            {
+                file = new FileStream("Data.txt", FileMode.Open, FileAccess.Read);
+                reader = new StreamReader(file);
+
+                data += reader.ReadLine(); // in case of multiple lines of data, we choose to append them
+
+                reader.Close();
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Data read from file : \n\n");
+                Console.WriteLine(data);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Writing to file/Writing to file/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// for StreamWriter 
+using System.IO;
+
+namespace Writing_to_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "Hello! If you're reading this inside a file, this program has done it's job successfully!";
+            StreamWriter writer;
+            FileStream file;
+
+            // let's use exception handling
+            // it's a good practice in handling files since
+            // this will make error tracking easier
+
+            try
+            {
+                file = new FileStream("Data.txt", FileMode.Create, FileAccess.Write);
+                writer = new StreamWriter(file);
+                writer.WriteLine(data);
+
+                // a good practice to close all the 
+                // resources
+                writer.Close();
+                file.Close(); 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nData successfully written to file Data.txt");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
